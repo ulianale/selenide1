08413502ead200f271cf3845d1e091e78eaf9edb
Uliana Le <ulianochka29@mail.ru>
2022-09-10 03:01:12 +0300

    add new tests

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -7,52 +8,83 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CallbackTest {
 
-    @Test
-    void test1() {
+    @BeforeEach
+    void setUp() {
         open("http://localhost:9999");
+    }
 
-        $("[data-test-id=name] input").setValue("Игорь Игорев");
+    @Test
+    void shouldValidForm() {
+        $("[data-test-id=name] input").setValue("Игорь Иго-игорев");
         $("[data-test-id=phone] input").setValue("+79099001111");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
-    void test2() {
-        open("http://localhost:9999");
-
+    void shouldNotValidName() {
         $("[data-test-id=name] input").setValue("Igor");
         $("[data-test-id=phone] input").setValue("+79099001111");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-
         $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-
     }
 
     @Test
-    void test3() {
-        open("http://localhost:9999");
-
+    void shouldEmptyName() {
+        $("[data-test-id=phone] input").setValue("+79099001111");
+        $("[data-test-id=agreement]").click();
         $(".button").click();
-
         $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
-
     }
 
     @Test
-    void test4() {
-        open("http://localhost:9999");
+    void shouldNotValidPhoneWithoutPlus() {
+        $("[data-test-id=name] input").setValue("Игорь Игорев");
+        $("[data-test-id=phone] input").setValue("79099001111");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
 
+    @Test
+    void shouldNotValidPhoneMoreNumber() {
+        $("[data-test-id=name] input").setValue("Игорь Игорев");
+        $("[data-test-id=phone] input").setValue("+790012345678");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldNotValidPhone() {
+        $("[data-test-id=name] input").setValue("Игорь Игорев");
+        $("[data-test-id=phone] input").setValue("Игорь");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldEmptyPhone() {
         $("[data-test-id=name] input").setValue("Игорь Игорев");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-
         $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
-
     }
 
+    @Test
+    void shouldNoCheckBox() {
+        $("[data-test-id=name] input").setValue("Игорь Игорев");
+        $("[data-test-id=phone] input").setValue("+79099001111");
+        $(".button").click();
+        $(".checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
 
+    @Test
+    void shouldEmptyForm() {
+        $(".button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
 }
